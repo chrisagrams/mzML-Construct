fdfea14b56a4028142847bfa515b7155a0e1e8af
Chris Grams <37988265+chrisagrams@users.noreply.github.com>
2024-10-21 13:21:48 -0500

    Added command line arguments

--- construct.py
+++ construct.py
@@ -1,3 +1,4 @@
+import argparse
 from lxml import etree
 from tqdm import tqdm
 import numpy as np
@@ -6,19 +7,35 @@ from utils.binary_utils import encode_binary
 from utils.file_utils import import_from_binary, import_from_parquet
 
 
-if __name__ == "__main__":
-    source = 'output/test.xml'
-    source_binary = 'output/test.parquet'
-    output_file = 'output/test.mzML'
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Construct mzML from XML and binary/parquet files.")
+    parser.add_argument("xml_file", help="Path to the input XML file")
+    parser.add_argument("data_file", help="Path to the input data file (Parquet or binary)")
+    parser.add_argument("output_file", help="Path to the output mzML file")
+    return parser.parse_args()
 
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    source = args.xml_file
+    source_data = args.data_file
+    output_file = args.output_file
+
+    # Parse the XML file
     tree = etree.parse(source)
     root = tree.getroot()
     nsmap = root.nsmap
     namespace = nsmap.get(None)
 
-    # df = import_from_parquet(source)
-    df = import_from_binary('output/test.bin')
+    # Determine the type of the data file and import accordingly
+    if source_data.endswith('.parquet'):
+        df = import_from_parquet(source_data)
+    elif source_data.endswith('.bin'):
+        df = import_from_binary(source_data)
+    else:
+        raise ValueError("The data file must be either a Parquet (.parquet) or binary (.bin) file")
 
+    # Process each spectrum in the XML
     for spectrum in tqdm(root.findall(f'.//{{{namespace}}}spectrum'), desc='Constructing mzML'):
         index = int(spectrum.get('index'))
         spectrum.set('defaultArrayLength', str(len(df['mz'][index])))
@@ -36,4 +53,5 @@ if __name__ == "__main__":
                     binary.text = encode_binary(np.array(df['int'][index]), source_format, source_compression)
                     binaryDataArray.attrib['encodedLength'] = str(len(binary.text))
 
+    # Write the modified XML tree to the output mzML file
     tree.write(output_file, pretty_print=True, xml_declaration=True, encoding='utf-8')
